b9d4332cd70d2d485279ad9f8c2717f1433935c5
Xavier BIZOUX <xavier.bizouxsas.com>
2021-07-06 11:31:54 +0200

    Blog_3

---
+++ src/components/Selectors/CASSelector.js
@@ -0,0 +1,89 @@
+import React, { useEffect, useState, useContext, Fragment } from 'react';
+
+import Instance from '../../apis/Instance';
+
+import { AuthContext } from '../../contexts';
+
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+
+import LibrarySelector from './LibrarySelector';
+import TableSelector from './TableSelector';
+import Loading from '../Loading';
+
+function CASSelector(props) {
+    const { authInfo } = useContext(AuthContext);
+    const [libraries, setLibraries] = useState([]);
+    const [tables, setTables] = useState([]);
+    const [selectedLibrary, setSelectedLibrary] = useState(null);
+    const [selectedTable, setSelectedTable] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const getSelectedLibrary = (selected) => {
+        setSelectedLibrary(selected.target.value);
+    }
+    const getSelectedTable = (selected) => {
+        setSelectedTable(selected.target.value);
+    }
+
+    useEffect(() => {
+        setLoading(true);
+        if ("cas" in authInfo.session && authInfo.session.cas !== undefined) {
+            const endpoint = `/cas-shared-default-http/cas/sessions/${authInfo.session.cas}/actions/table.caslibInfo`;
+            const headers = {
+                "accept": "application/json",
+                "content-type": "application/json"
+            };
+            Instance.post(endpoint, { headers: headers })
+                .then(response => {
+                    setLibraries(response.data.results.CASLibInfo.rows);
+                    setLoading(false);
+                });
+        }
+    }, [authInfo.session, setLoading]);
+
+    useEffect(() => {
+        setLoading(true);
+        setSelectedTable(null);
+        setTables([]);
+        if (selectedLibrary !== null) {
+            const endpoint = `/cas-shared-default-http/cas/sessions/${authInfo.session.cas}/actions/table.tableInfo`;
+            const headers = {
+                "accept": "application/json",
+                "content-type": "application/json"
+            };
+            const data = { "caslib": selectedLibrary };
+            Instance.post(endpoint, data, { headers: headers })
+                .then(response => {
+                    if ("results" in response.data) {
+                        setTables(response.data.results.TableInfo.rows);
+                    }
+                    setLoading(false);
+                });
+        }
+    }, [selectedLibrary, authInfo.session.cas]);
+
+    return (
+        <Fragment>
+            <Form onSubmit={props.onSelect} style={{ padding: 10 }}>
+                <Form.Row>
+                    <Col sm="4">
+                        <LibrarySelector data={libraries} type="CAS" handleSelection={getSelectedLibrary} />
+                    </Col>
+                    <Col sm="7">
+                        <TableSelector data={tables} type="CAS" disabled={selectedLibrary === null ? true : false} handleSelection={getSelectedTable} />
+                    </Col>
+                    <Col sm="1">
+                        <Button variant="primary" type="submit" disabled={selectedTable === null ? true : false} >
+                            Display
+                    </Button>
+                    </Col>
+                </Form.Row>
+            </Form>
+            <Loading status={loading} />
+        </Fragment>
+    )
+}
+
+export default CASSelector;
--- src/pages/Cas.js
+++ src/pages/Cas.js
@@ -1,20 +1,75 @@
-import { useEffect, useContext } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useEffect, useState, useContext } from 'react';
+import { useHistory} from 'react-router-dom';
+import Instance from '../apis/Instance';
 
 import { AuthContext } from '../contexts';
 
-function Cas() {
-    const { authInfo } = useContext(AuthContext);
+import Container from 'react-bootstrap/Container';
+
+import CASSelector from '../components/Selectors/CASSelector';
+import TableViewer from '../components/TableViewer/TableViewer';
+import Loading from '../components/Loading';
+
+function CAS() {
+    const { authInfo, setAuthInfo } = useContext(AuthContext);
+    const [selection, setSelection] = useState({ library: null, table: null });
+    const [tableData, setTableData] = useState({ rows: [], columns: [] });
+    const [loading, setLoading] = useState(false);
     const history = useHistory();
 
     useEffect(() => {
         if (authInfo.authenticated === false) {
             history.push("/logon");
         }
-    }, [authInfo.authenticated, history]);
-    return (
-        <h2>CAS page</h2>
-    );
-}
+    },[authInfo.authenticated, history]);
 
-export default Cas;
+    useEffect(() => {
+        if (authInfo.authenticated === true && !("cas" in authInfo.session) ) {
+            const endpoint = '/cas-shared-default-http/cas/sessions';
+            Instance.post(endpoint)
+                .then(response => {
+                    setAuthInfo({ ...authInfo, session: { ...authInfo.session, cas: response.data.session }});
+                })
+        }
+    }, [authInfo, setAuthInfo]);
+
+    useEffect(() => {
+        if (authInfo.session.cas !== "" && selection.table !== null) {
+            const endpoint = `/cas-shared-default-http/cas/sessions/${authInfo.session.cas}/actions/table.fetch`;
+            const data = {
+                "table":{
+                    "caslib":selection.library,
+                    "name":selection.table
+                }
+            }
+            const headers = {
+                "accept": "application/json",
+                "content-type": "application/json"
+            };
+            Instance.post(endpoint, data,{headers: headers} )
+                .then(response => {
+                    if (response.data.results.Fetch.schema.length > 0) {
+                        const result = response.data.results.Fetch;
+                        setTableData({ rows: result.rows, columns: result.schema });
+                    }
+                });
+        }
+    }, [selection, authInfo.session]);
+
+    const handleSelection = (event) => {
+        event.preventDefault();
+        setTableData({ rows: [], columns: [] });
+        setLoading(true);
+        const selection = { library: event.target[0].value, table: event.target[1].value };
+        setSelection(selection);
+    };
+
+    return (
+        <Container className="justify-content-md-center">
+            <CASSelector onSelect={handleSelection}/>
+            {tableData.rows.length !== tableData.columns.length ? <TableViewer data={tableData} /> : <Loading status={loading} />}
+        </Container>
+    )
+};
+
+export default CAS;
